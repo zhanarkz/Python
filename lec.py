cefed1ac8bd3c4e2b8e160c85856f5a1735f0ef6
Zhanar <zhanarkz@gmail.com>
2022-12-13 00:02:44 +0600

    seminar 1

---
+++ lec.py
@@ -0,0 +1,16 @@
+print('enter a')
+a = int(input())
+print('enter b')
+b = int(input())
+#s = '"hello World"'
+#print(s) # вывод строки
+print('{1} - {0}'.format(a, b))
+print(f'{a} - {b}')
+print(a, '+', b, "=", a+b)
+
+
+#f = False
+#print(f)
+
+#list = [1, 2, 3,] #строка
+#print(list)
