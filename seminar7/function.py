379e7f622f42fca2f3b407bd2ec449ab3f705f34
Zhanar <zhanarkz@gmail.com>
2023-02-04 22:29:56 +0600

    HW seminar7

--- seminar7/function.py
+++ seminar7/function.py
@@ -2,7 +2,8 @@ def read_data_from_file(name):
     result = []
     with open(name, 'r', encoding='utf8') as datafile:
         for line in datafile:
-            result.append(line.strip('\n').split(','))
+            line = line.replace('\n', '')
+            result.append(line.split(','))
         return result
 
 def save_data_to_file(name, data_list):
@@ -10,7 +11,11 @@ def save_data_to_file(name, data_list):
         datafile.write(data_list +'\n')
 
 def print_bus():
-    return read_data_from_file('bus.txt')
+    buses_list = read_data_from_file('bus.txt')
+    print('|№ |\tавтобус ID |\t тип  |\t гос. номер')
+    print('-'*60)
+    for num, bus in enumerate(buses_list):
+        print(f'|{num+1} |\t     {bus[0]} |\t {bus[2]}|\t{bus[1]}')
 
 def add_bus():
     save_data_to_file('bus.txt', input("введите параметры автобуса "))
@@ -21,8 +26,26 @@ def print_driver():
 def add_driver():
     save_data_to_file('driver.txt', input("введите данные водителя "))
 
+from operator import itemgetter
+def get_data(request: str, data_list: list) -> list:
+    '''возвращает нужную строку, содержащую запрос'''
+    for line in data_list:
+        if request == itemgetter(0)(line):
+            return line
+    return ['None', 'None', 'None', 'None']
+
 def print_route():
-    return read_data_from_file('route.txt')
+    routes_list = read_data_from_file('route.txt')
+    buses_list = read_data_from_file('bus.txt')
+    print('|№ |\tмаршрут |\tавтобус |\tгос.номер | маршрут')
+    print('-'*70)
+    for num, route in enumerate(routes_list):
+        bus_request = route[1]
+        bus_data = get_data(bus_request, buses_list)
+        print(
+             f'|{num+1} |\t{route[0]} |\t{bus_data[0]}|'
+             f'\t{bus_data[1]} | {route[1]}')
+   
 
 def add_route():
     save_data_to_file('route.txt', input("введите данные маршрута "))
